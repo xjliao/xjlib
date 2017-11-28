b2fcea04b3e144d38d8249003a237c9cbba12e6f
xjl <lxj990@gmail.com>
2017-11-28 14:36:00 +0800

    Added XResponseStatus

--- xjlib/src/main/java/me/xjliao/xjlib/net/XResponse.java
+++ xjlib/src/main/java/me/xjliao/xjlib/net/XResponse.java
@@ -58,6 +58,30 @@ public class XResponse<T> {
             this.code = code;
             this.msg = msg;
         }
+
+        public int getCode() {
+            return code;
+        }
+
+        public void setCode(int code) {
+            this.code = code;
+        }
+
+        public String getMsg() {
+            return msg;
+        }
+
+        public void setMsg(String msg) {
+            this.msg = msg;
+        }
+
+        @Override
+        public String toString() {
+            return "XResponseStatus{" +
+                    "code=" + code +
+                    ", msg='" + msg + '\'' +
+                    '}';
+        }
     }
 }
 
