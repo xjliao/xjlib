ddb0a150a520461c908538044cfc2b5757470936
xjl <lxj990@gmail.com>
2018-01-18 10:56:10 +0800

    Added XNonSwipeableViewPager

---
+++ xjlib/src/main/java/me/xjliao/xjlib/widget/XNonSwipeableViewPager.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2017 xjliao.me created by xjliao
+ * ProjectName: xjl
+ * ModuleName: xjlib
+ * FileName: XNonSwipeableViewPager.java
+ * ClassName: XNonSwipeableViewPager
+ * LastModified: 1/18/18 10:51 AM
+ */
+
+package me.xjliao.xjlib.widget;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+public class XNonSwipeableViewPager extends ViewPager {
+
+    private boolean enabled;
+
+    public XNonSwipeableViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.enabled = true;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (this.enabled) {
+            return super.onTouchEvent(event);
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        if (this.enabled) {
+            return super.onInterceptTouchEvent(event);
+        }
+
+        return false;
+    }
+
+    public void setPagingEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+}
