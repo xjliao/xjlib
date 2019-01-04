79ce51401b5a13e0acab9d1a1f6f08be9fcef1e1
xjl <lxj990@gmail.com>
2019-01-04 12:01:22 +0800

    A

--- android/app/src/main/java/me/xjliao/xjl/view/XXReactActivity.java
+++ android/app/src/main/java/me/xjliao/xjl/view/XXReactActivity.java
@@ -1,11 +1,18 @@
 package me.xjliao.xjl.view;
 
+import android.os.Bundle;
+
 import com.microsoft.codepush.react.CodePush;
 
 import me.xjliao.xjl.BuildConfig;
 import me.xjliao.xjlib.react.XReactActivity;
 
 public class XXReactActivity extends XReactActivity {
+
+	@Override
+	public void setArgLaunchOptions(Bundle bundle) {
+
+	}
 
 	@Override
 	public void initReactInstanceManagerBuilder() {
