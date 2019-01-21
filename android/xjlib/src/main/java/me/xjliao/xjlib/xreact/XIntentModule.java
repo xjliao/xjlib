c95e458dc5f8dc2c04f7f566219bbc92c01b687b
xjl <lxj990@gmail.com>
2019-01-21 17:03:35 +0800

    Optimized react

--- android/xjlib/src/main/java/me/xjliao/xjlib/xreact/XIntentModule.java
+++ android/xjlib/src/main/java/me/xjliao/xjlib/xreact/XIntentModule.java
@@ -7,6 +7,9 @@ import com.facebook.react.bridge.JSApplicationIllegalArgumentException;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.ReadableMapKeySetIterator;
+import com.facebook.react.bridge.ReadableType;
 
 public class XIntentModule extends ReactContextBaseJavaModule {
 
@@ -16,23 +19,52 @@ public class XIntentModule extends ReactContextBaseJavaModule {
 
 	@Override
 	public String getName() {
-		return  XReactActivity.ARG_COMPONENT_NAME;
+		return "XIntentModule";
 	}
 
 	@ReactMethod
-	public void startActivityFromJS(String activityFullPath, String params){
+	public void startActivityFromJS(String activityFullPath, ReadableMap params){
 		try{
 			Activity currentActivity = getCurrentActivity();
 			if(null!=currentActivity){
 				Class toActivity = Class.forName(activityFullPath);
 				Intent intent = new Intent(currentActivity,toActivity);
-				intent.putExtra("params", params);
+				intentPutExtra(intent,  params);
 				currentActivity.startActivity(intent);
 			}
 		}catch(Exception e){
 			throw new JSApplicationIllegalArgumentException(
-					"不能打开Activity : "+e.getMessage());
+					"不能打开Activity : " + activityFullPath + ":" +e.getMessage());
 		}
 	}
 
+	public void intentPutExtra(Intent intent, ReadableMap params) {
+		System.out.println(params);
+		ReadableMapKeySetIterator iterator = params.keySetIterator();
+
+		while (iterator.hasNextKey()) {
+			String key = iterator.nextKey();
+			ReadableType type = params.getType(key);
+
+			switch (type) {
+				case Null:
+					intent.putExtra(key, "");
+					break;
+				case Boolean:
+					intent.putExtra(key, params.getBoolean(key));
+					break;
+				case Number:
+					intent.putExtra(key, params.getDouble(key));
+					break;
+				case String:
+					intent.putExtra(key, params.getString(key));
+					break;
+			}
+		}
+	}
+
+	@Override
+	public boolean canOverrideExistingModule() {
+		return true;
+	}
 }
---
+++ android/xjlib/src/main/java/me/xjliao/xjlib/xreact/XPackage.java
@@ -0,0 +1,29 @@
+package me.xjliao.xjlib.xreact;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public  abstract class XPackage implements ReactPackage {
+
+	private List<NativeModule> nativeModules = new ArrayList<>();
+
+	@Override
+	public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+		addModules(nativeModules,  reactContext);
+		return nativeModules;
+	}
+
+	@Override
+	public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+		return Collections.emptyList();
+	}
+
+	public abstract  void addModules(List<NativeModule> nativeModules, ReactApplicationContext reactContext);
+
+}
